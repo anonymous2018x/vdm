537e753c0cf79dcf457b0f83000f56952a9d86d7
Anonymous2018x <40073825+anonymous2018x@users.noreply.github.com>
2019-08-18 15:02:37 +0300

    have created db config file

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-	$dbServer = '94.130.51.39';
-	$dbUsername = 'reducepo_Admin';
+	$dbServer = 'localhost';
+	$dbUsername = 'root';
 	$dbPassword = 'DennoBrown450';
-	$dbDatabase = 'reducepo_vdm';
+	$dbDatabase = 'vdm';
 
 	$adminConfig = array(
 		'adminUsername' => "denno",
@@ -15,16 +15,16 @@
 		'membersPerPage' => "10",
 		'recordsPerPage' => "10",
 		'custom1' => "Full Name",
-		'custom2' => "County",
-		'custom3' => "Location",
-		'custom4' => "Village",
+		'custom2' => "Address",
+		'custom3' => "City",
+		'custom4' => "State",
 		'MySQLDateFormat' => "%m/%d/%Y",
 		'PHPDateFormat' => "n/j/Y",
 		'PHPDateTimeFormat' => "m/d/Y, h:i a",
 		'senderName' => "Membership management",
 		'senderEmail' => "poasanabrown@gmail.com",
 		'approvalSubject' => "Your membership is now approved",
-		'approvalMessage' => "Dear member,\n\nYour membership is now approved by the admin. You can log in to your account here:\nhttp://reducepovertyclub.com\n\nRegards,\nAdmin",
+		'approvalMessage' => "Dear member,\n\nYour membership is now approved by the admin. You can log in to your account here:\nhttp://localhost:8080\n\nRegards,\nAdmin",
 		'hide_twitter_feed' => "true",
 		'maintenance_mode_message' => "<b>Our website is currently down for maintenance</b><br>\r\nWe expect to be back in a couple hours. Thanks for your patience.",
 		'mail_function' => "mail",
